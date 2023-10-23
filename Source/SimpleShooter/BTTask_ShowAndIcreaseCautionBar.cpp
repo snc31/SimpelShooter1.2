7219620267e85edeed380433e9b6e38814310730
snc31 <85165300+snc31@users.noreply.github.com>
2023-10-23 21:34:57 +0100

    CautionBarClassSetup

---
+++ Source/SimpleShooter/BTTask_ShowAndIcreaseCautionBar.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_ShowAndIcreaseCautionBar.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "ShooterAIController.h"
+#include "ShooterCharacter.h"
+
+UBTTask_ShowAndIcreaseCautionBar::UBTTask_ShowAndIcreaseCautionBar()
+{
+	NodeName = "Increase Caution Bar";
+}
+
+EBTNodeResult::Type UBTTask_ShowAndIcreaseCautionBar::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return EBTNodeResult::InProgress;
+}
+
+void UBTTask_ShowAndIcreaseCautionBar::ShowCautionBar(UBehaviorTreeComponent& OwnerComp)
+{
+	AAIController* OwnerController = OwnerComp.GetAIOwner(); //get the AI controller thats connected to the behaviour tree
+	AShooterCharacter* ControlledCharacter = Cast<AShooterCharacter>(OwnerController->GetPawn()); //Cast to the shooter character type and get the pawn enemy.
+}
---
+++ Source/SimpleShooter/BTTask_ShowAndIcreaseCautionBar.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_ShowAndIcreaseCautionBar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLESHOOTER_API UBTTask_ShowAndIcreaseCautionBar : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+public:
+	UBTTask_ShowAndIcreaseCautionBar();
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)override;
+
+private:
+	void ShowCautionBar(UBehaviorTreeComponent& OwnerComp);
+};
